50984a733e47a7ba32a5365496f16ba01a83bff1
Lucas Godoy <lgodoysilva@gmail.com>
2022-09-20 15:47:38 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se concectando ao banco de dados")
-// mais um console log de teste:
+// mais um console log de teste1:
 console.log("O Paulo fez esse commit sem voce ver na aula!")
